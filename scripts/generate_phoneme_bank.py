f0923c484399190c14865d10dc2e931f205de5f8
NiLuJe <ninuje@gmail.com>
2024-12-08 20:09:10 +0100

    Test de la création en Python

--- scripts/generate_phoneme_bank.py
+++ scripts/generate_phoneme_bank.py
@@ -3,28 +3,30 @@
 # See also: gruut-ipa, ipasymbols, and, most importantly: panphon
 import ipapy
 
-for p in ipapy.IPA_CHARS:
-	print(f"{p}: {p.name} {type(p)}")
-	if p.is_vowel:
-		print(p)
-		print(p.height)
-		print(p.backness)
-		print(p.roundness)
-		if p.modifiers:
-			print(" ".join(p.modifiers))
-	elif p.is_consonant:
-		print(p)
-		print(p.voicing)
-		print(p.manner)
-		print(p.place)
-		if p.modifiers:
-			print(" ".join(p.modifiers))
-	elif p.is_diacritic:
-		print(p)
-		print(p.name.replace(" diacritic", ""))
-	elif p.is_suprasegmental:
-		print(p)
-		print(p.name.replace(" suprasegmental", ""))
-	elif p.is_tone:
-		print(p)
-		print(p.name.replace(" tone", ""))
+def print_phoneme_bank():
+	"""Print the phoneme bank to stdout"""
+	for p in ipapy.IPA_CHARS:
+		print(f"{p}: {p.name} {type(p)}")
+		if p.is_vowel:
+			print(p)
+			print(p.height)
+			print(p.backness)
+			print(p.roundness)
+			if p.modifiers:
+				print(" ".join(p.modifiers))
+		elif p.is_consonant:
+			print(p)
+			print(p.voicing)
+			print(p.manner)
+			print(p.place)
+			if p.modifiers:
+				print(" ".join(p.modifiers))
+		elif p.is_diacritic:
+			print(p)
+			print(p.name.replace(" diacritic", ""))
+		elif p.is_suprasegmental:
+			print(p)
+			print(p.name.replace(" suprasegmental", ""))
+		elif p.is_tone:
+			print(p)
+			print(p.name.replace(" tone", ""))
