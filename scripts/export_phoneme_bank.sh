d756a7fbfea67d740c13c662b5a63a2f979fe761
NiLuJe <ninuje@gmail.com>
2024-12-12 20:55:19 +0100

    Lookup the Feature name if the PhonemeBank dump

--- scripts/export_phoneme_bank.sh
+++ scripts/export_phoneme_bank.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sqlite3 -header -csv DB/conlangs.db "SELECT * FROM PhonemeBank;" > data/PhonemeBank.csv
+# Lookup Feature
+sqlite3 -header -csv DB/conlangs.db "SELECT PhonemeID, IPA, Type, Vowel_Height, Vowel_Backness, Vowel_Roundness, Consonant_Voicing, Consonant_ArticulationManner, Consonant_ArticulationPlace, Modifiers, PhonemeFeature.Name AS Feature FROM PhonemeBank LEFT JOIN PhonemeFeature ON PhonemeBank.Feature = PhonemeFeature.ID;" > data/PhonemeBank.csv
