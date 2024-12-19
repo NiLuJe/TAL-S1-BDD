7811ac825889237abaa7d3f77f2a1f2bd04fe555
NiLuJe <ninuje@gmail.com>
2024-12-20 00:33:23 +0100

    Make the PhonemeBank export query actually readable

--- scripts/export_phoneme_bank.sh
+++ scripts/export_phoneme_bank.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
 # Lookup Feature
-sqlite3 -header -csv DB/conlangs.db "SELECT PhonemeID, IPA, Type, Vowel_Height, Vowel_Backness, Vowel_Roundness, Consonant_Voicing, Consonant_ArticulationManner, Consonant_ArticulationPlace, Modifiers, PhonemeFeature.Name AS Feature FROM PhonemeBank LEFT JOIN PhonemeFeature ON PhonemeBank.Feature = PhonemeFeature.ID;" > data/PhonemeBank-Exported.csv
+QUERY="
+SELECT
+	PhonemeID,
+	IPA,
+	Type,
+	Vowel_Height,
+	Vowel_Backness,
+	Vowel_Roundness,
+	Consonant_Voicing,
+	Consonant_ArticulationManner,
+	Consonant_ArticulationPlace,
+	Modifiers,
+	PhonemeFeature.Name AS Feature
+FROM
+	PhonemeBank
+	LEFT JOIN PhonemeFeature ON PhonemeBank.Feature = PhonemeFeature.ID;
+"
+sqlite3 -header -csv DB/conlangs.db "${QUERY}" > data/PhonemeBank-Exported.csv
 
 # Export PhonemeFeature, too, since we add stuff to it during PhonemeBank creation
 sqlite3 -header -csv DB/conlangs.db "SELECT * FROM PhonemeFeature;" > data/PhonemeFeature-Exported.csv
